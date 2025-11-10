0c63929171a5b1a769935528081e91d0c19cbcc0
nvhuy249 <109942049+nvhuy249@users.noreply.github.com>
2025-11-10 12:29:08 +1030

    add suspense to side nav

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,13 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Suspense } from 'react';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
-        <SideNav />
+        <Suspense fallback={<div>Loading nav...</div>}></Suspense>
+          <SideNav />
+        <Suspense />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
