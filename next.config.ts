e2f8e01d8763843194e214353e0e2f04ce7d60f1
nvhuy249 <109942049+nvhuy249@users.noreply.github.com>
2025-11-10 11:33:06 +1030

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    cacheComponents: true
-  }
+  },
+  cacheComponents: true
 };
 
 export default nextConfig;
